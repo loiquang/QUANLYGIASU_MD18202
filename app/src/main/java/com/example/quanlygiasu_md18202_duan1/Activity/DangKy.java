fa27e194fce8482b6a538033c6c963586ed5897e
loiquang <loidqps27284@fpt.edu.vn>
2023-07-05 16:56:54 +0700

    layout_DangKy_DangNhap_Cho

---
+++ app/src/main/java/com/example/quanlygiasu_md18202_duan1/Activity/DangKy.java
@@ -0,0 +1,16 @@
+package com.example.quanlygiasu_md18202_duan1.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.quanlygiasu_md18202_duan1.R;
+
+public class DangKy extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dang_ky);
+    }
+}
---
+++ app/src/main/java/com/example/quanlygiasu_md18202_duan1/FragMent/fragment_timkiemgiasu.java
@@ -0,0 +1,23 @@
+package com.example.quanlygiasu_md18202_duan1.FragMent;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.quanlygiasu_md18202_duan1.R;
+
+public class fragment_timkiemgiasu extends Fragment {
+
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_timkiemgiasu, container, false);
+    }
+}
