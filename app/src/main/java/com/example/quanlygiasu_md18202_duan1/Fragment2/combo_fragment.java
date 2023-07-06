3fe6429f7c62eea9cd8ae5d99f9622769c685b23
loiquang <loidqps27284@fpt.edu.vn>
2023-07-06 17:35:13 +0700

    layout_profile_done_giasu_not

--- app/src/main/java/com/example/quanlygiasu_md18202_duan1/Fragment2/combo_fragment.java
+++ app/src/main/java/com/example/quanlygiasu_md18202_duan1/Fragment2/combo_fragment.java
@@ -16,7 +16,7 @@ public class combo_fragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.combo_fragment, container, false);
+        View view = inflater.inflate(R.layout.combo_fragment, container, false);
+        return view;
     }
 }
--- app/src/main/java/com/example/quanlygiasu_md18202_duan1/Fragment2/giasu_fragment.java
+++ app/src/main/java/com/example/quanlygiasu_md18202_duan1/Fragment2/giasu_fragment.java
@@ -7,6 +7,7 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.SearchView;
 
 import com.example.quanlygiasu_md18202_duan1.R;
 
@@ -17,6 +18,9 @@ public class giasu_fragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.giasu_fragment, container, false);
+        View view = inflater.inflate(R.layout.giasu_fragment, container, false);
+         SearchView searchView = view.findViewById(R.id.searchView);
+        searchView.setIconifiedByDefault(false);
+        return view;
     }
 }
