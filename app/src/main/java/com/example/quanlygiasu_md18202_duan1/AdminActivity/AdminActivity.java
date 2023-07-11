2f36b2a4c578c940023ed7d93f4c38029790e313
TheQ <108474084+ThanhQuoc0501@users.noreply.github.com>
2023-07-11 15:09:25 +0700

    up_giao_dien_admin

---
+++ app/src/main/java/com/example/quanlygiasu_md18202_duan1/AdminActivity/AdminActivity.java
@@ -0,0 +1,16 @@
+package com.example.quanlygiasu_md18202_duan1.AdminActivity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.quanlygiasu_md18202_duan1.R;
+
+public class AdminActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_admin);
+    }
+}
