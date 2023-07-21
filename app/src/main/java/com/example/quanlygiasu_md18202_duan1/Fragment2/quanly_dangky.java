e414bced3d467858aa12e91fd352d59554be21b8
loiquang <loidqps27284@fpt.edu.vn>
2023-07-21 13:22:54 +0700

    up_Dang_ky_GV

---
+++ app/src/main/java/com/example/quanlygiasu_md18202_duan1/Fragment2/quanly_dangky.java
@@ -0,0 +1,70 @@
+package com.example.quanlygiasu_md18202_duan1.Fragment2;
+
+import static android.content.Context.MODE_PRIVATE;
+
+import android.content.SharedPreferences;
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.example.quanlygiasu_md18202_duan1.Activity.GiaSuCuaBan;
+import com.example.quanlygiasu_md18202_duan1.Adapter.Giasucuaban;
+import com.example.quanlygiasu_md18202_duan1.FireBaseHelper.GetListFireBase;
+import com.example.quanlygiasu_md18202_duan1.InterFace.Interface_list;
+import com.example.quanlygiasu_md18202_duan1.Models.Request.ReQuestGS;
+import com.example.quanlygiasu_md18202_duan1.Models.Teacher_Models.Teacher_MD;
+import com.example.quanlygiasu_md18202_duan1.Models.users.User;
+import com.example.quanlygiasu_md18202_duan1.R;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.ArrayList;
+
+
+public class quanly_dangky extends Fragment {
+private RecyclerView recyclerView;
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_quanly_dangky, container, false);
+        recyclerView = view.findViewById(R.id.recycleView);
+        GetListFireBase getListFireBase = new GetListFireBase();
+        FirebaseDatabase firebaseDatabase = FirebaseDatabase.getInstance();
+        DatabaseReference databaseReference = firebaseDatabase.getReference("request");
+        getListFireBase.readDatabase3(databaseReference, new Interface_list() {
+            @Override
+            public void onListReceived(ArrayList<Teacher_MD> list) {
+
+            }
+            @Override
+            public void onListReceived1(ArrayList<User> list) {}
+            @Override
+            public void onListReceived2(ArrayList<ReQuestGS> list) {
+                ArrayList<ReQuestGS> list2 = new ArrayList<>();
+
+
+                for (ReQuestGS reQuestGS : list) {
+                    if(reQuestGS.getStatus()==0){
+                        list2.add(reQuestGS);
+                    }
+                }
+                Giasucuaban giasucuaban = new Giasucuaban(list2);
+                LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getActivity());
+                recyclerView.setLayoutManager(linearLayoutManager);
+                recyclerView.setAdapter(giasucuaban);
+            }
+        // Inflate the layout for this fragment
+
+    });
+        return view;
+    }
+    }
