13045e412908b788908bcc299f9a0298b7c0a898
TheQ <108474084+ThanhQuoc0501@users.noreply.github.com>
2023-08-05 11:11:26 +0700

    up_thongtin_activity

---
+++ app/src/main/java/com/example/quanlygiasu_md18202_duan1/AdminActivity/AdminActivity/ThongTinActivity.java
@@ -0,0 +1,14 @@
+package com.example.quanlygiasu_md18202_duan1.AdminActivity.AdminActivity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ThongTinActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_thong_tin);
+    }
+}
